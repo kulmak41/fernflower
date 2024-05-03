5f92dbdb95ce03a9395809f5e83f283853253bc6
Makar Kuleshov <M.Kuleshov@student.tudelft.nl>
2024-05-04 01:21:21 +0200

    Rename the class to match the file name

--- testData/src/records/TestRecordGeneratedMethods.java
+++ testData/src/records/TestRecordGeneratedMethods.java
@@ -7,11 +7,12 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE_USE)
 @interface TA {}
 
-public record TestRecordSyntheticMethods(Integer a, Double b, Boolean c) {
-  public TestRecordSyntheticMethods(String a, String b, String c) {
+public record TestRecordGeneratedMethods(Integer a, Double b, Boolean c) {
+  public TestRecordGeneratedMethods(String a, String b, String c) {
     this(Integer.parseInt(a), Double.parseDouble(b), Boolean.parseBoolean(c));
   }
-  public TestRecordSyntheticMethods(Integer x) {
+
+  public TestRecordGeneratedMethods(Integer x) {
     this(x, 2.0 * x, false);
   }
 
